120e00dbe9cec627a93c43cea3815261f5b80d3a
Bryce Stradling <bryce@getvyral.com>
2015-12-29 16:25:18 -0600

    base styles and layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log("you got it baby.");
+window.timekitBookingConfig = {
+      name: 'Rhonda Sher',
+      email: 'linkedindiva@gmail.com',
+      apiToken: '2EYkGBQZrhV76uBQn2u8Zwg8K0ONC30p',
+      calendar: 'adfe7677-0446-4c85-9a67-676aa844b4ca',
+      avatar: 'https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg',
+      timekitFindTime: {
+        filters: {
+          and: [
+            {
+              business_hours: { }
+            },
+            {
+              exclude_weekend: { }
+            }
+          ],
+          or: [
+            {
+              specific_time: {
+                start: '00',
+                end: '12'
+              }
+            },
+            {
+              specific_time: {
+                start: '13',
+                end: '24'
+              }
+            }
+          ]
+        }
+      }
+    }
